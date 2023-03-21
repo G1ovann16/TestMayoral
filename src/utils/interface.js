b6f397131b357bf9c5c058c98ae0a2a672c04fca
Gio <glandaburo92@gmail.com>
2023-03-21 16:07:07 +0100

    craeted structure

---
+++ src/utils/interface.js
@@ -0,0 +1,11 @@
+interface Schema  {
+    id: number;
+    title: string;
+    img: string;
+    price: number;
+    newPrice: number | null;
+    category: string;
+  };
+  
+
+  export default Schema
